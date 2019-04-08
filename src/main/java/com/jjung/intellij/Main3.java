e21a3f30ebff811d903ecd9e3eee3c135d14aa26
jjung <jjung@jjungui-MacBookAir.local>
2019-04-08 17:08:51 +0900

    Initial commit

---
+++ src/main/java/com/jjung/intellij/Main3.java
@@ -0,0 +1,34 @@
+package com.jjung.intellij;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("qqq33qqq" + "여기까지가 끝인 가보오" + "이렇게 이렇게 이렇게 이렇게");
+        System.out.println("qqq222qqq");
+        System.out.println("qqq222qqq");
+        System.out.println("qqq222qqq");
+
+        System.out.println();
+
+        Calendar calendar = new GregorianCalendar();
+        if (calendar == null) {
+            
+        }
+    }
+    public String age;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void countPaes(int page){
+        System.out.println(page);
+        System.out.println(page);
+    }
+}
